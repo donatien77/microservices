9d6bfc25985e3cdbc9131f0773cabd799083ef7a
donatien <macosmonterey@macOSs-MBP.hitronhub.home>
2024-04-06 14:41:45 -0400

    first commit

---
+++ product-service/src/test/java/com/donatien/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donatien.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
