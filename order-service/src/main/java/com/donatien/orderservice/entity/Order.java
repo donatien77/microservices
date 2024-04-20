e0bfe36f92a1f2c3e722efbc0fc34491029f40d9
donatien <macosmonterey@macOSs-MBP.hitronhub.home>
2024-04-20 13:49:36 -0400

    Adding Config Client in Order and Product Service

--- order-service/src/main/java/com/donatien/orderservice/entity/Order.java
+++ order-service/src/main/java/com/donatien/orderservice/entity/Order.java
@@ -1,10 +1,12 @@
 package com.donatien.orderservice.entity;
 
+
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 
 import java.time.Instant;
 
