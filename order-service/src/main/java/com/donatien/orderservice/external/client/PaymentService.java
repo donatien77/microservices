7cae6f5771da8151bd67cd766adb965aac3a973a
donatien <macosmonterey@macOSs-MBP.hitronhub.home>
2024-05-17 21:09:43 -0400

    Implementing Circuit Breaker in Order Service

--- order-service/src/main/java/com/donatien/orderservice/external/client/PaymentService.java
+++ order-service/src/main/java/com/donatien/orderservice/external/client/PaymentService.java
@@ -1,6 +1,8 @@
 package com.donatien.orderservice.external.client;
 
+import com.donatien.orderservice.exception.CustomException;
 import com.donatien.orderservice.external.request.PaymentRequest;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,9 +13,14 @@ import org.springframework.web.bind.annotation.RequestBody;
  * User macosmonterey
  * Date 05 May, 2024
  */
+@CircuitBreaker(name = "external", fallbackMethod = "fallback")
 @FeignClient(name = "PAYMENT-SERVICE/payment")
 public interface PaymentService {
 
     @PostMapping("/doPayment")
     public ResponseEntity<Long> doPayment (@RequestBody PaymentRequest paymentRequest);
+
+    default void fallback(Exception e){
+        throw new CustomException("Payment service is not available", "UNAVAILABLE", 500);
+    }
 }
--- order-service/src/main/java/com/donatien/orderservice/external/client/ProductService.java
+++ order-service/src/main/java/com/donatien/orderservice/external/client/ProductService.java
@@ -1,5 +1,7 @@
 package com.donatien.orderservice.external.client;
 
+import com.donatien.orderservice.exception.CustomException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,9 +13,14 @@ import org.springframework.web.bind.annotation.RequestParam;
  * User macosmonterey
  * Date 26 Apr, 2024
  */
+@CircuitBreaker(name = "external", fallbackMethod = "fallback")
 @FeignClient(name = "PRODUCT-SERVICE/product")
 public interface ProductService {
 
     @PutMapping("/reduceQuantity/{id}")
     ResponseEntity<Void> reduceQuantity(@PathVariable("id") Long productId, @RequestParam Long quantity);
+
+    default void fallback(Exception e){
+        throw new CustomException("Product service is not available", "UNAVAILABLE", 500);
+    }
 }
